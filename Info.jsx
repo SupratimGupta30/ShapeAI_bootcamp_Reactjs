94d58befc474bb267bb474479338c86e75249f9a
SupratimGupta30 <86525050+SupratimGupta30@users.noreply.github.com>
2021-06-26 20:05:28 +0530

    Add files via upload

---
+++ Info.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+function Info() {
+  return (
+    <div className="note">
+     <h1>Javascript ans React.js</h1>
+     <p>basic web dev React js Bootcamp</p>
+   </div>
+  );
+}
+
+export default Info;
